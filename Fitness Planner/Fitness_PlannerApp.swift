7075086def72cdb35d2293457086bdebfebda45f
Cihan Hasanoglu <cihan.hasanoglu@sap.com>
2022-11-08 14:51:50 +0100

    Initial Commit

---
+++ Fitness Planner/Fitness_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_PlannerApp.swift
+//  Fitness Planner
+//
+//  Created by Hasanoglu, Cihan on 08.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
